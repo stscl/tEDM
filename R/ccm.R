b8cc40581a4174bbe7932ff27f1d368aaadb30ca
SpatLyu <lyu.geosocial@gmail.com>
2025-06-18 13:52:43 +0800

    implement ccm s4 generic

---
+++ R/ccm.R
@@ -0,0 +1,59 @@
+.ccm_ts_method = \(data, cause, effect, libsizes, E = 3, tau = 1, k = E+2, theta = 1, algorithm = "simplex", lib = NULL, pred = NULL,
+                    threads = detectThreads(), parallel.level = "low", bidirectional = TRUE, progressbar = TRUE){
+  E = .check_inputelementnum(E,2)
+  tau = .check_inputelementnum(tau,2)
+  k = .check_inputelementnum(k,2)
+  pl = .check_parallellevel(parallel.level)
+  if (is.null(lib)) lib = .internal_library(data)
+  if (is.null(pred)) pred = lib
+  cause = .uni_ts(data,cause)
+  effect = .uni_ts(data,effect)
+
+  simplex = ifelse(algorithm == "simplex", TRUE, FALSE)
+  x_xmap_y = NULL
+  if (bidirectional){
+    x_xmap_y = RcppCCM(cause,effect,libsizes,lib,pred,E[1],tau[1],k[1],simplex,theta,threads,pl,progressbar)
+  }
+  y_xmap_x = RcppCCM(effect,cause,libsizes,lib,pred,E[2],tau[2],k[2],simplex,theta,threads,pl,progressbar)
+
+  return(.bind_xmapdf(varname,x_xmap_y,y_xmap_x,bidirectional))
+}
+
+#' convergent cross mapping
+#'
+#' @param data observation data.
+#' @param cause name of causal variable.
+#' @param effect name of effect variable.
+#' @param libsizes number of time points used in prediction.
+#' @param E (optional) embedding dimensions.
+#' @param tau (optional) step of time lags.
+#' @param k (optional) number of nearest neighbors used in prediction.
+#' @param theta (optional) weighting parameter for distances, useful when `algorithm` is `smap`.
+#' @param algorithm (optional) prediction algorithm.
+#' @param lib (optional) libraries indices.
+#' @param pred (optional) predictions indices.
+#' @param threads (optional) number of threads to use.
+#' @param parallel.level (optional) level of parallelism, `low` or `high`.
+#' @param bidirectional (optional) whether to examine bidirectional causality.
+#' @param progressbar (optional) whether to show the progress bar.
+#'
+#' @return A list
+#' \describe{
+#' \item{\code{xmap}}{cross mapping results}
+#' \item{\code{varname}}{names of causal and effect variable}
+#' \item{\code{bidirectional}}{whether to examine bidirectional causality}
+#' }
+#' @export
+#' @name ccm
+#' @aliases ccm,ts-method
+#' @references
+#' Gao, B., Yang, J., Chen, Z. et al. Causal inference from cross-sectional earth system data with geographical convergent cross mapping. Nat Commun 14, 5875 (2023).
+#'
+#' @examples
+#' columbus = sf::read_sf(system.file("case/columbus.gpkg", package="spEDM"))
+#' \donttest{
+#' g = gccm(columbus,"hoval","crime",libsizes = seq(5,45,5),E = 6)
+#' g
+#' plot(g, ylimits = c(0,0.85))
+#' }
+methods::setMethod("ccm", "data.frame", .ccm_ts_method)
--- R/internal_utility.R
+++ R/internal_utility.R
@@ -27,6 +27,11 @@
   return(pl)
 }
 
+.internal_varname = \(conds = NULL){
+  .varname = c("cause","effect")
+  return(.varname)
+}
+
 .internal_library = \(data){
   nnaindice = which(apply(is.na(data),1,\(.x) !any(.x)))
   return(nnaindice)
@@ -42,5 +47,65 @@
 .multivar_ts = \(data,columns){
   columns = .check_character(columns)
   res = as.matrix(data[,columns,drop = FALSE])
+  return(res)
+}
+
+.internal_xmapdf_binding = \(varname,x_xmap_y,y_xmap_x,bidirectional,keyname = "libsizes"){
+  colnames(y_xmap_x) = c(keyname,"y_xmap_x_mean","y_xmap_x_sig",
+                         "y_xmap_x_upper","y_xmap_x_lower")
+  y_xmap_x = as.data.frame(y_xmap_x)
+
+  if (bidirectional){
+    colnames(x_xmap_y) = c(keyname,"x_xmap_y_mean","x_xmap_y_sig",
+                           "x_xmap_y_upper","x_xmap_y_lower")
+    x_xmap_y = as.data.frame(x_xmap_y)
+    resdf = x_xmap_y |>
+      dplyr::full_join(y_xmap_x, by = keyname) |>
+      dplyr::arrange({{keyname}})
+  } else {
+    resdf = dplyr::arrange(y_xmap_x,{{keyname}})
+  }
+
+  return(resdf)
+}
+
+.bind_xmapdf = \(varname,x_xmap_y,y_xmap_x,bidirectional){
+  resdf = .internal_xmapdf_binding(varname,x_xmap_y,y_xmap_x,bidirectional)
+  res = list("xmap" = resdf, "varname" = varname, "bidirectional" = bidirectional)
+  class(res) = 'ccm_res'
+  return(res)
+}
+
+.bind_xmapdf2 = \(varname,x_xmap_y,y_xmap_x,bidirectional){
+
+  tyxmapx = y_xmap_x[,c(1,2,4:6),drop = FALSE]
+  dyxmapx = y_xmap_x[,c(1,3,7:9),drop = FALSE]
+  txxmapy = NULL
+  dxxmapy = NULL
+  if(bidirectional){
+    txxmapy = x_xmap_y[,c(1,2,4:6),drop = FALSE]
+    dxxmapy = x_xmap_y[,c(1,3,7:9),drop = FALSE]
+  }
+
+  txmap = .internal_xmapdf_binding(varname[1:2],txxmapy,tyxmapx,bidirectional)
+  dxmap = .internal_xmapdf_binding(varname[1:2],dxxmapy,dyxmapx,bidirectional)
+
+  res = list("pxmap" = dxmap, "xmap" = txmap,
+             "varname" = varname[1:2],
+             "bidirectional" = bidirectional)
+  class(res) = 'pcm_res'
+  return(res)
+}
+
+.bind_intersectdf = \(varname,x_xmap_y,y_xmap_x,bidirectional){
+  resdf = .internal_xmapdf_binding(varname,x_xmap_y,y_xmap_x,bidirectional,keyname = "neighbors")
+  res = list("xmap" = resdf, "varname" = varname, "bidirectional" = bidirectional)
+  class(res) = 'cmc_res'
+  return(res)
+}
+
+.bind_xmapself = \(x,varname,...){
+  res = list("xmap" = x,"varname" = varname)
+  class(res) = "xmap_self"
   return(res)
 }

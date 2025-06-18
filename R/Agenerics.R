8f9bc7c80b1a4a35e0ca624d9786fa37c4c26e27
SpatLyu <lyu.geosocial@gmail.com>
2025-06-18 13:22:21 +0800

    configure s4 generics of `tEDM` package

---
+++ R/Agenerics.R
@@ -0,0 +1,12 @@
+register_generic = \(name, def = NULL) {
+  if (!methods::isGeneric(name)){
+    if (is.null(def)) {
+      def = eval(bquote(function(data, ...) standardGeneric(.(name))))
+    }
+    methods::setGeneric(name, def)
+  }
+}
+
+for (gen in c("embedded", "ccm", "pcm")) {
+  register_generic(gen)
+}

07f78aa9b0a05c337975f8748e6f9d99970b540e
SpatLyu <lyu.geosocial@gmail.com>
2025-06-19 21:52:16 +0800

    synchronize logisitc map updates

--- R/pcm.R
+++ R/pcm.R
@@ -42,10 +42,10 @@
 #' Leng, S., Ma, H., Kurths, J. et al. Partial cross mapping eliminates indirect causal influences. Nat Commun 11, 2632 (2020).
 #'
 #' @examples
-#' sim = as.data.frame(logistic_map(x = 0.4,y = 0.4,z = 0.4,step = 45,
-#'                                  beta_xy = 0.5, beta_xz = 0,
-#'                                  beta_yx = 0, beta_yz = 0.5,
-#'                                  beta_zx = 0, beta_zy = 0))
+#' sim = logistic_map(x = 0.4,y = 0.4,z = 0.4,step = 45,
+#'                    beta_xy = 0.5, beta_xz = 0,
+#'                    beta_yx = 0, beta_yz = 0.5,
+#'                    beta_zx = 0, beta_zy = 0)
 #' pcm(sim,"x","z","y",libsizes = seq(5,35,5),E = 10,k = 7,threads = 1)
 #'
 methods::setMethod("pcm", "data.frame", .scpcm_ts_method)

14fec5850cc7e132bb149fc653290cfef1add506
SpatLyu <lyu.geosocial@gmail.com>
2025-07-02 01:32:27 +0800

    clean cmd check notes

--- R/globals.R
+++ R/globals.R
@@ -1,3 +1,4 @@
 utils::globalVariables(c("libsizes","x_xmap_y_mean", "y_xmap_x_mean",
                          "x_xmap_y_lower", "x_xmap_y_upper",
-                         "y_xmap_x_lower", "y_xmap_x_upper"))
+                         "y_xmap_x_lower", "y_xmap_x_upper",
+                         "x_xmap_y_sig", "y_xmap_x_sig"))

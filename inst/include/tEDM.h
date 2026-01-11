b48f308027e0ea33497610c9852226f373e41803
Dirk Eddelbuettel <edd@debian.org>
2026-01-11 08:43:00 -0600

    Use inst/include/tEDM.h to enfore include order in RcppExport.cpp

---
+++ inst/include/tEDM.h
@@ -0,0 +1,6 @@
+
+// This file will be include into the generated file RcppExports.cpp
+// allowing us to specifiy include order, and (if needed) declare types
+
+#include <RcppArmadillo.h>
+#include <RcppThread.h>

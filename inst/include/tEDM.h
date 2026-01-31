455eb191be4f37efd5e20d25da6d3268ab9f2d6c
SpatLyu <lyu.geosocial@gmail.com>
2026-01-31 23:50:07 +0800

    implement include guard in tEDM.h

--- inst/include/tEDM.h
+++ inst/include/tEDM.h
@@ -1,6 +1,10 @@
+#ifndef tEDM_H
+#define tEDM_H
 
 // This file will be include into the generated file RcppExports.cpp
-// allowing us to specifiy include order, and (if needed) declare types
+// allowing us to specify include order, and (if needed) declare types
 
 #include <RcppArmadillo.h>
 #include <RcppThread.h>
+
+#endif // tEDM_H
